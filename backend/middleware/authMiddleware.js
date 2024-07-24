dad5e188c4db96e8eb87b32315044748adf9181e
manos-alex <manosalex22@gmail.com>
2024-07-24 19:00:55 -0400

    fix(backend): fix authMiddleware.js and complete example.env

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,12 +2,12 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
 module.exports = async (req, res, next) => {
+    try {
     const accessToken = req.cookies.accessToken;
     if (!token) {
         return res.status(401).json({ message: 'No token, authorization denied' });
     }
 
-    try {
         const decoded = jwt.verify(accessToken, process.env.JWT_SECRET);
         req.userId = decoded.userId;
         const user = await User.findById(req.userId);
