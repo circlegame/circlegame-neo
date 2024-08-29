21167ed668254857e3224aea04530322616b21a5
josef-karpinski <joeykarp@comcast.net>
2024-08-29 19:40:22 -0400

    chore: update footer version for release 0.3.1

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -18,7 +18,7 @@ function Footer() {
         <FooterContainer>
             <FooterDiv className={gameState === 'pregame' || gameState === 'endgame' ? "" : 'hidden'}>
                 <Version>
-                    v0.3.0
+                    v0.3.1
                 </Version>
                 <FooterButtons>
                     <a href="https://github.com/circlegame/circlegame-neo" target="_blank" rel="noreferrer noopener">
